eb257884a39533989a487f8112eae0e90e98b4a5
philippkionke <vuztyM-comhy0-fywcos>
2022-04-28 19:15:11 +0200

    Abgabe PSE

---
+++ Berechnung.js
@@ -0,0 +1,11 @@
+function Berechnung() {
+  var a = 2;
+  var b = 2;
+  var c = 1;
+
+  var ergebniss1 = a + b;
+  var ergebniss2 = ergebniss1 * c;
+  var ergebnissText = a + " + " + b + " = " + ergebniss1 + " * " + c + " = " + ergebniss2;
+
+  console.log(ergebnissText);
+}
